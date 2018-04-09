821115eef3a2d3142d3e460a7c6a285bb07ef363
Joël Spaltenstein <spalte@naturalimage.ch>
2018-04-09 17:52:28 +0200

    Added a listener that starts repeating task

---
+++ src/main/java/online/kheops/auth_server/SeriesFetchContextListener.java
@@ -0,0 +1,29 @@
+package online.kheops.auth_server;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@WebListener
+public class SeriesFetchContextListener implements ServletContextListener {
+    private static final int MAXIMUM_CONCURRENT = 1;
+
+    private ScheduledThreadPoolExecutor executor = null;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("contextInitialized");
+        executor = new ScheduledThreadPoolExecutor(MAXIMUM_CONCURRENT);
+
+        SeriesFetchTask task = new SeriesFetchTask();
+        executor.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed");
+        executor.shutdown();
+    }
+}
---
+++ src/main/java/online/kheops/auth_server/SeriesFetchTask.java
@@ -0,0 +1,8 @@
+package online.kheops.auth_server;
+
+public class SeriesFetchTask implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("fetching series");
+    }
+}
