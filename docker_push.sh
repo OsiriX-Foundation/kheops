3689c43e9bec8677db4ce88fd305bbc281b52cae
gerome12 <gerome.pasquier@gmail.com>
2018-05-11 17:07:47 +0200

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -3,7 +3,9 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 mv build/libs/KheopsAuthorization.war docker/KheopsAuthorization.war
 
-docker build ./docker/ -t test
+docker build ./docker/ -t osirixfoundation/kheopsauthorization-tomcat
 docker image ls
+
+docker push osirixfoundation/kheopsauthorization-tomcat
 
 find -name "*.war"
