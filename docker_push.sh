c95bca72d337fd0fa6989b8f1a75466b0a028a55
gerome12 <gerome.pasquier@gmail.com>
2018-05-11 16:35:31 +0200

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker build ./docker/ -t osirixfoundation/test
+docker build ./docker/ -t test/test
+docker image ls
+#.build/libs/KheopsAuthorization.war
 find -name "*.war"
