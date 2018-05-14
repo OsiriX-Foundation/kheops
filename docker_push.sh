7dca14f58ea33cea19388f8d357a51723306edc4
gerome12 <gerome.pasquier@gmail.com>
2018-05-14 08:45:03 +0200

    [ci skip]

--- docker_push.sh
+++ docker_push.sh
@@ -4,8 +4,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 mv build/libs/KheopsAuthorization.war docker/KheopsAuthorization.war
 
 docker build ./docker/ -t osirixfoundation/kheopsauthorization-tomcat
-docker image ls
 
 docker push osirixfoundation/kheopsauthorization-tomcat
-
-find -name "*.war"
