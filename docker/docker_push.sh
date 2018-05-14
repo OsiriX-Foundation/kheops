79d64d31239ccc7d25d7049da7f557a5219c4588
gerome12 <gerome.pasquier@gmail.com>
2018-05-14 08:48:49 +0200

    [ci skip]

---
+++ docker/docker_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+mv build/libs/KheopsAuthorization.war docker/KheopsAuthorization.war
+
+docker build ./docker/ -t osirixfoundation/kheopsauthorization-tomcat
+
+docker push osirixfoundation/kheopsauthorization-tomcat
