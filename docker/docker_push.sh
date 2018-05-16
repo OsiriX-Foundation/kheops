11b5867a5e9aba46d75ab92c58a6a6e09b583314
gerome12 <gerome.pasquier@gmail.com>
2018-05-16 16:16:47 +0200

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -3,6 +3,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 mv build/libs/KheopsAuthorization.war docker/KheopsAuthorization.war
 
-docker build ./docker/ -t osirixfoundation/kheopsauthorization-tomcat
+docker build ./docker/ -t osirixfoundation/kheopsauthorization-tomcat:$BRANCH
 
-docker push osirixfoundation/kheopsauthorization-tomcat
+docker push osirixfoundation/kheopsauthorization-tomcat:$BRANCH
