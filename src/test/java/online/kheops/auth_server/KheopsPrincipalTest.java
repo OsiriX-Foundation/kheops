5a8413ee6f34119c1ef1abe7b02c5cfcd62d5fde
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-23 11:51:28 +0200

    a real test

--- src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
+++ src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
@@ -1,16 +1,30 @@
 package online.kheops.auth_server;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KheopsPrincipalTest {
 
+    private final static long principalDBID = 123456789L;
+    private final static String principalName = "123456789";
+
+    private KheopsPrincipal kheopsPrincipal;
+
+    @BeforeAll
+    void beforeAll() {
+        kheopsPrincipal = new KheopsPrincipal(principalDBID);
+    }
+
     @Test
-    void justAnExample() {
-        System.out.println("This test method should be run");
-        assertEquals(true, false);
+    void getDBID() {
+        assertEquals(principalDBID, kheopsPrincipal.getDBID());
+    }
+
+    @Test
+    void getName() {
+        assertEquals(principalName, kheopsPrincipal.getName());
     }
 
 }
