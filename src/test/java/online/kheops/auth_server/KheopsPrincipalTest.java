b54a6d94be10f7bd9661f6cb34338dc8adeb6347
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-23 11:31:42 +0200

    more playing with tests

--- src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
+++ src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
@@ -1,5 +1,16 @@
+package online.kheops.auth_server;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KheopsPrincipalTest {
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+        assertEquals(true, true);
+    }
 
 }
