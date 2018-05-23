aaa2755126a69d0a0e411fff47562ff85a7b58e2
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-23 11:34:58 +0200

    made the test fail

--- src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
+++ src/test/java/online/kheops/auth_server/KheopsPrincipalTest.java
@@ -10,7 +10,7 @@ class KheopsPrincipalTest {
     @Test
     void justAnExample() {
         System.out.println("This test method should be run");
-        assertEquals(true, true);
+        assertEquals(true, false);
     }
 
 }
