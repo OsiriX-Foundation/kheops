66b0197e74bc005cd2f0ae2b55247b28c58495c8
gerome12 <gerome.pasquier@gmail.com>
2018-05-24 16:56:07 +0200

    Create ep.sh

---
+++ src/main/resources/sql/ep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+filename="/run/secrets/mysql_user"
+user=$(head -n 1 $filename)
+
+filename="/run/secrets/mysql_password"
+pwd=$(head -n 1 $filename)
+
+
+mysql -uroot -p123456<<MYSQL_SCRIPT
+
+CREATE USER '$user'@'%' IDENTIFIED BY '$pwd';
+GRANT ALL ON *.* TO '$user'@'%';
+
+MYSQL_SCRIPT
+
