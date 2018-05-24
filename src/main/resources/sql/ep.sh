12c13ba8bd78d5f9762c93c02340b374a1bfd0cd
gerome12 <gerome.pasquier@gmail.com>
2018-05-24 17:16:49 +0200

    Update ep.sh

--- src/main/resources/sql/ep.sh
+++ src/main/resources/sql/ep.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-filename="/run/secrets/mysql_user"
+filename="/run/secrets/my_mysql_user"
 user=$(head -n 1 $filename)
 
-filename="/run/secrets/mysql_password"
+filename="/run/secrets/my_mysql_password"
 pwd=$(head -n 1 $filename)
 
-mysql -uroot -p123456<<MYSQL_SCRIPT
+filename="/run/secrets/mysql_root_password"
+root_pwd=$(head -n 1 $filename)
+
+mysql -uroot -p$rootpwd<<MYSQL_SCRIPT
 
 CREATE USER '$user'@'%' IDENTIFIED BY '$pwd';
 GRANT ALL ON *.* TO '$user'@'%';
