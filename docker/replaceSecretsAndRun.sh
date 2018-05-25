37fad1c09a370439d72f892a8b67f945ec47620a
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 13:06:44 +0200

    Update replaceSecretsAndRun.sh

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -4,7 +4,12 @@ for f in ${SECRET_FILE_PATH}
 do
   filename=$(basename "$f")
   value=$(cat ${f})
+  echo $value
+  echo $filename
   sed -i "s/\${$filename}/$value/" ${REPLACE_FILE_PATH}
 done
 
 catalina.sh run;
+
+
+echo endscript
