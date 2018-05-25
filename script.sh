4df57e54e89f6f5ffdb838cbcef1a68f9fbcbe74
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 16:42:51 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ chmod a+w /etc/nginx/conf.d/myconf.conf
 sed -i "s|\${root_url}|$ROOT_URL|" /etc/nginx/conf.d/myconf.conf
 
 https="https://"
-server_name=${$ROOT_URL/$https/}
+server_name=${ROOT_URL/$https/}
 
 echo $server_name
 cat /etc/nginx/conf.d/myconf.conf
