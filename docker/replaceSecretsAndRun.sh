abc57d61ceb3c63fd115154d2799e41e467a8d2b
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 14:20:04 +0200

    Update replaceSecretsAndRun.sh

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -7,4 +7,8 @@ do
   sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
 done
 
+sed -i "s|\${kheops_mysql_user}|$KHEOPS_MYSQL_USER|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_mysql_url}|$KHEOPS_MYSQL_URL|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_pacs_url}|$KHEOPS_PACS_URL|" ${REPLACE_FILE_PATH}
+
 catalina.sh run;
