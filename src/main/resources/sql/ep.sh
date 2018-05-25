e80f25954d096a2c249b556996a75f33a462ad28
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 09:00:10 +0200

    Update ep.sh

--- src/main/resources/sql/ep.sh
+++ src/main/resources/sql/ep.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-filename="/run/secrets/mysql_user"
-user=$(head -n 1 $filename)
 
-filename="/run/secrets/mysql_password"
-pwd=$(head -n 1 $filename)
+if [ -f /run/secrets/mysql_user ]; then
+    filename="/run/secrets/mysql_user"
+    user=$(head -n 1 $filename)
+else
+    user=default_user
+fi
 
-filename="/run/secrets/mysql_root_password"
-rootpwd=$(head -n 1 $filename)
+if [ -f /run/secrets/mysql_password ]; then
+    filename="/run/secrets/mysql_password"
+    pwd=$(head -n 1 $filename)
+else
+    pwd=default_pwd
+fi
+
+if [ -f /run/secrets/mysql_root_password ]; then
+    filename="/run/secrets/mysql_root_password"
+    rootpwd=$(head -n 1 $filename)
+else
+    rootpwd=rootpwd
+fi
+
 
 mysql -uroot -p$rootpwd<<MYSQL_SCRIPT
 
-CREATE USER '$user'@'tomcataut' IDENTIFIED BY '$pwd';
-GRANT ALL ON kheops.* TO '$user'@'tomcataut';
+CREATE USER '$user'@'%' IDENTIFIED BY '$pwd';
+GRANT ALL ON kheops.* TO '$user'@'%';
 
 MYSQL_SCRIPT
