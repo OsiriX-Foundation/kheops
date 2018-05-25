93ef012c8700d6135cd74e62d071ee42f153d27a
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 17:02:07 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-
 chmod a+w /etc/nginx/conf.d/myconf.conf
 sed -i "s|\${root_url}|$ROOT_URL|" /etc/nginx/conf.d/myconf.conf
 
 https="https://"
 server_name=${ROOT_URL/$https/}
 
-echo $server_name
-
-
 sed -i "s|\${server_name}|$server_name|" /etc/nginx/conf.d/myconf.conf
-cat /etc/nginx/conf.d/myconf.conf
-
 
 nginx -g 'daemon off;'
