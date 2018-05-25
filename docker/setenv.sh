2588bb6ce172d399ded163ef82719f56a33ab0cc
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 11:49:26 +0200

    Update setenv.sh

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
 
 echo setenvsetenvsetenv
-export FOO=fooooooooooooooooo
+
+if [ -f /run/secrets/kheops_superuser_hmasecret ]; then
+    filename="/run/secrets/kheops_superuser_hmasecret"
+    superuser_hmasecret=$(head -n 1 $filename)
+else
+    superuser_hmasecret=XXX
+fi
+
+
+export KHEOPS_SUPERUSER_HMASECRET=$superuser_hmasecret
+#export KHEOPS_AUTH_HMASECRET=$auth_hmasecret
+#export KHEOPS_JDBC_USER=$jdbc_user
+#export KHEOPS_JDBC_PASSWORD=$jdbc_user
