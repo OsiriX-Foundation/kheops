17f12292371cf77db8d9894d622f6a0003c2a933
gerome12 <gerome.pasquier@gmail.com>
2018-05-25 14:23:16 +0200

    [ci skip]

--- src/main/resources/sql/ep.sh
+++ src/main/resources/sql/ep.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if [ -f /run/secrets/mysql_user ]; then
-    filename="/run/secrets/mysql_user"
-    user=$(head -n 1 $filename)
-else
-    user=default_user
-fi
+user=$KHEOPS_MYSQL_USER
 
 if [ -f /run/secrets/mysql_password ]; then
     filename="/run/secrets/mysql_password"
     pwd=$(head -n 1 $filename)
 else
+    echo "Missing kheops mysql_password secret"
     pwd=default_pwd
 fi
 
@@ -18,6 +14,7 @@ if [ -f /run/secrets/mysql_root_password ]; then
     filename="/run/secrets/mysql_root_password"
     rootpwd=$(head -n 1 $filename)
 else
+    echo "Missing kheops mysql_root_password secret"
     rootpwd=rootpwd
 fi
 
