9d4da769a4b67981a668e1b956d706722083923a
gerome12 <gerome.pasquier@gmail.com>
2018-05-28 17:50:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 chmod a+w /etc/nginx/conf.d/myconf.conf
-sed -i "s|\${root_url}|$ROOT_URL|" /etc/nginx/conf.d/myconf.conf
+sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/myconf.conf
 
 https="https://"
-server_name=${ROOT_URL/$https/}
+server_name=${KHEOPS_ROOT_URL/$https/}
 
 sed -i "s|\${server_name}|$server_name|" /etc/nginx/conf.d/myconf.conf
 
