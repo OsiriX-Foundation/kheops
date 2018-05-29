4d8bd78dbff10c816bbbd95f0aaa5a2f1c101dac
gerome12 <gerome.pasquier@gmail.com>
2018-05-29 09:29:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-chmod a+w /etc/nginx/conf.d/myconf.conf
-sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/myconf.conf
+chmod a+w /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
 
-sed -i "s|\${kheopsviewer_url}|$KHEOPS_NGINX_URL_KHEOPSVIEWER|" /etc/nginx/conf.d/myconf.conf
-sed -i "s|\${tomcat_url}|$KHEOPS_NGINX_URL_TOMCAT|" /etc/nginx/conf.d/myconf.conf
+sed -i "s|\${kheopsviewer_url}|$KHEOPS_NGINX_URL_KHEOPSVIEWER|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${tomcat_url}|$KHEOPS_NGINX_URL_TOMCAT|" /etc/nginx/conf.d/kheops.conf
 
 https="https://"
 server_name=${KHEOPS_ROOT_URL/$https/}
 
-sed -i "s|\${server_name}|$server_name|" /etc/nginx/conf.d/myconf.conf
+sed -i "s|\${server_name}|$server_name|" /etc/nginx/conf.d/kheops.conf
 
 nginx -g 'daemon off;'
