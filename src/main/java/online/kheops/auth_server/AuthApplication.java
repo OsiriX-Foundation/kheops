fde543bed90cbdd77953f05085ed666925a2276a
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-30 14:28:18 +0200

    removed setting of ServerProperties.OUTBOUND_CONTENT_LENGTH_BUFFER

--- src/main/java/online/kheops/auth_server/AuthApplication.java
+++ src/main/java/online/kheops/auth_server/AuthApplication.java
@@ -1,18 +1,9 @@
 package online.kheops.auth_server;
 
-import org.glassfish.jersey.server.ServerProperties;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashMap;
-import java.util.Map;
 
 @ApplicationPath("/*")
 public class AuthApplication extends Application {
-    @Override
-    public Map<String, Object> getProperties() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ServerProperties.OUTBOUND_CONTENT_LENGTH_BUFFER, 0);
-        return props;
-    }
+
 }
