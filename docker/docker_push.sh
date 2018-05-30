7203d1e6e61b576ab2f9104bd9021a693513404d
gerome12 <gerome.pasquier@gmail.com>
2018-05-30 10:58:10 +0200

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -6,3 +6,6 @@ mv build/libs/KheopsAuthorization.war docker/KheopsAuthorization.war
 docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheopsauthorization-tomcat:$BRANCH
 
 docker push osirixfoundation/kheopsauthorization-tomcat:$BRANCH
+
+
+curl -X POST https://hooks.microbadger.com/images/osirixfoundation/kheopsauthorization-tomcat/Io28UtBZLy58gv5hZzlxFkhPdy0=
