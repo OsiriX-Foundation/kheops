2a6053b21e35852c9f788209d36ca0e503c8ed27
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-31 16:40:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
 
 sed -i "s|\${kheopsviewer_url}|$KHEOPS_NGINX_URL_KHEOPSVIEWER|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${tomcat_url}|$KHEOPS_NGINX_URL_TOMCAT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${proxy_url}|$KHEOPS_NGINX_URL_PROXY|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${wado_url}|$KHEOPS_WADO_ROOT|" /etc/nginx/conf.d/kheops.conf
 
 https="https://"
