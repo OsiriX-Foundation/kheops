f257bc68cd201b96052dc63752b89491cae1dd83
Joël Spaltenstein <spalte@naturalimage.ch>
2018-05-31 16:25:29 +0200

    commit

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-mv build/libs/KheopsProxy.war docker/KheopsProxy.war
+mv build/libs/KheopsDICOMwebProxy.war docker/KheopsDICOMwebProxy.war
 
 docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheopsproxy-tomcat:$BRANCH
 
