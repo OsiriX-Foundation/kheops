9b88e9522998976420694dc86b0be5a3666792cd
gerome12 <gerome.pasquier@gmail.com>
2018-06-07 11:35:31 +0200

    Update stop_proxy_container.sh

--- scripts/stop_proxy_container.sh
+++ scripts/stop_proxy_container.sh
@@ -6,9 +6,8 @@ set -e
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 . $script_dir/common.sh
 
-proxy_name=$1
 proxy_dir=$proxy_base_dir/$proxy_name
 
-echo -e "${cyan}Stopping the '$proxy_name' container and removing the image${no_color}"
-docker rm -f "osirixfoundation/pacsproxyauthorization" &>/dev/null || true
+echo -e "${cyan}Stopping the proxy container and removing the image${no_color}"
+docker rm -f "proxy_default" &>/dev/null || true
 docker rmi -f "osirixfoundation/pacsproxyauthorization" &>/dev/null || true
