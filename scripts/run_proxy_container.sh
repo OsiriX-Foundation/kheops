7b2e213b6323198f79d15c4060fdd78300b67f59
gerome12 <gerome.pasquier@gmail.com>
2018-06-07 12:59:27 +0200

    Update run_proxy_container.sh

--- scripts/run_proxy_container.sh
+++ scripts/run_proxy_container.sh
@@ -7,4 +7,6 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 . $script_dir/common.sh. 
 . $script_dir/build_proxy_container.sh
 
+
+echo -e "${cyan}RUN docker image${no_color}"
 docker run --name "proxy-default" -d -p 80:80 --link backend:backend "osirixfoundation/pacsproxyauthorization"
