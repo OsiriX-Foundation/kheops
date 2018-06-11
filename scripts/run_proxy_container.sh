9c38cbc4f5a64a1f1bb32b501bb0a88c8cf38672
gerome12 <gerome.pasquier@gmail.com>
2018-06-11 16:37:07 +0200

    [ci skip]

--- scripts/run_proxy_container.sh
+++ scripts/run_proxy_container.sh
@@ -3,10 +3,5 @@
 set -o pipefail
 set -e
 
-script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-. $script_dir/common.sh
-. $script_dir/build_proxy_container.sh
-
-
 echo -e "${cyan}RUN docker image${no_color}"
 docker run --name "proxy-default" -d -p 80:80 --link backend:backend "osirixfoundation/pacsproxyauthorization"
