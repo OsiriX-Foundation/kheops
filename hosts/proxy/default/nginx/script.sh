efab348a750381ce4285ee752e952e80e9924624
gerome12 <gerome.pasquier@gmail.com>
2018-06-12 10:37:16 +0200

    Update script.sh

--- hosts/proxy/default/nginx/script.sh
+++ hosts/proxy/default/nginx/script.sh
@@ -5,4 +5,5 @@ chmod a+w /opt/openresty/nginx/conf/nginx.conf
 sed -i "s|\${pacs_wado_uri}|$KHEOPS_WADO_URI_ROOT|" /opt/openresty/nginx/conf/nginx.conf
 sed -i "s|\${pacs_wado_rs}|$KHEOPS_PACS_URL|" /opt/openresty/nginx/conf/nginx.conf
 
-nginx -g daemon off; error_log /dev/stderr info;
+nginx -g daemon off
+error_log /dev/stderr info
