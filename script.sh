ca7ebda7229bbc3c8538bb01643e0ab129c7075e
gerome12 <gerome.pasquier@gmail.com>
2018-06-14 13:54:57 +0200

    all on port 443 [ci skip]

--- script.sh
+++ script.sh
@@ -3,10 +3,10 @@
 chmod a+w /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
 
-sed -i "s|\${kheopsviewer_url}|$KHEOPS_NGINX_URL_KHEOPSVIEWER|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${tomcat_url}|$KHEOPS_NGINX_URL_TOMCAT|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${proxy_url}|$KHEOPS_NGINX_URL_PROXY|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${wado_url}|$KHEOPS_WADO_ROOT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_NGINX_URL_DICOMWEB_PROXY|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorizationProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION_PROXY|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsWebUI_url}|$KHEOPS_NGINX_URL_VIEWER|" /etc/nginx/conf.d/kheops.conf
 
 https="https://"
 server_name=${KHEOPS_ROOT_URL/$https/}
