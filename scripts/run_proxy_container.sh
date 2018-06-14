f013846d8e905aae48766a0a3ee520d0cebcd992
gerome12 <gerome.pasquier@gmail.com>
2018-06-14 16:52:05 +0200

    Delete run_proxy_container.sh

--- scripts/run_proxy_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o pipefail
-set -e
-
-echo -e "${cyan}RUN docker image${no_color}"
-docker run --name "proxy-default" -d -p 80:80 --link backend:backend "osirixfoundation/pacsproxyauthorization"
