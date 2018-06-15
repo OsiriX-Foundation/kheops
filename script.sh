f524ca0c73c5a4781bb2228d3724c7b28c652c3a
gerome12 <gerome.pasquier@gmail.com>
2018-06-15 09:53:01 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 chmod a+w /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
 
-sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_NGINX_URL_DICOMWEB_PROXY|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_NGINX_URL_DICOMWEB_PROXY|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_NGINX_URL_AUTHORIZATION|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsAuthorizationProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION_PROXY|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsWebUI_url}|$KHEOPS_NGINX_URL_VIEWER|" /etc/nginx/conf.d/kheops.conf
 
