755fd5853066da029a5c09b24bcb410cfa11400a
gerome12 <gerome.pasquier@gmail.com>
2018-06-20 14:38:59 +0200

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -3,6 +3,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 mv build/libs/KheopsDICOMwebProxy.war docker/KheopsDICOMwebProxy.war
 
-docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheopsproxy-tomcat:$BRANCH
+docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheops-dicomweb-proxy:$BRANCH
 
-docker push osirixfoundation/kheopsproxy-tomcat:$BRANCH
+docker push osirixfoundation/kheops-dicomweb-proxy:$BRANCH
