13358fae2c1b5256b4114de48d93ac53e4647d37
gerome12 <gerome.pasquier@gmail.com>
2018-06-21 10:18:33 +0200

    change hmasecret

--- hosts/proxy/default/nginx/script.sh
+++ hosts/proxy/default/nginx/script.sh
@@ -5,14 +5,14 @@ chmod a+w /opt/openresty/nginx/conf/nginx.conf
 sed -i "s|\${pacs_wado_uri}|$KHEOPS_PROXY_PACS_WADO_URI|" /opt/openresty/nginx/conf/nginx.conf
 sed -i "s|\${pacs_wado_rs}|$KHEOPS_PROXY_PACS_WADO_RS|" /opt/openresty/nginx/conf/nginx.conf
 
-if [ -f /run/secrets/kheops_superuser_hmasecret ]; then
-    filename="/run/secrets/kheops_superuser_hmasecret"
-    kheops_superuser_hmasecret=$(head -n 1 $filename)
+if [ -f /run/secrets/kheops_auth_hmasecret ]; then
+    filename="/run/secrets/kheops_auth_hmasecret"
+    kheops_auth_hmasecret=$(head -n 1 $filename)
 else
-    echo "Missing kheops superuser_hmasecret secret"
-    kheops_superuser_hmasecret=XXX
+    echo "Missing kheops auth_hmasecret secret"
+    kheops_auth_hmasecret=XXX
 fi
 
-export JWT_SECRET=$kheops_superuser_hmasecret
+export JWT_SECRET=$kheops_auth_hmasecret
 
 nginx -g 'daemon off; error_log /dev/stderr info;'
