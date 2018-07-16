5f75323f577d94d1261113137d8138eab01c9541
gerome12 <gerome.pasquier@gmail.com>
2018-07-16 13:51:33 +0200

    [ci skip] add zipper

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_NGINX_URL_DICOMWEB_PROXY|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_NGINX_URL_AUTHORIZATION|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsAuthorizationProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION_PROXY|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsZipper_url}|$KHEOPS_NGINX_URL_ZIPPER_PROXY|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsWebUI_url}|$KHEOPS_NGINX_URL_VIEWER|" /etc/nginx/conf.d/kheops.conf
 
 https="https://"
