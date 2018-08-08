955bf285eb730922e230f3d79a9d210d9c098152
gerome12 <gerome.pasquier@gmail.com>
2018-08-08 15:59:03 +0200

    remove unused code [ci skip]

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -1,14 +1,5 @@
 #! /bin/sh
 
-for f in ${SECRET_FILE_PATH}
-do
-  filename=$(basename "$f")
-  value=$(cat ${f})
-  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
-done
-
-sed -i "s|\${kheops_mysql_user}|$KHEOPS_MYSQL_USER|" ${REPLACE_FILE_PATH}
-sed -i "s|\${kheops_mysql_url}|$KHEOPS_MYSQL_URL|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_pacs_url}|$KHEOPS_PACS_URL|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_authorization_url}|$KHEOPS_AUTHORIZATION_URL|" ${REPLACE_FILE_PATH}
 
