91d081c128f9e53c96013886bef6d6f4d45576f9
gerome12 <gerome.pasquier@gmail.com>
2018-10-22 10:09:11 +0200

    Update UserPrincipal.java

--- src/main/java/online/kheops/auth_server/UserPrincipal.java
+++ src/main/java/online/kheops/auth_server/UserPrincipal.java
@@ -191,7 +191,7 @@ public class UserPrincipal implements KheopsPrincipalInterface {
             final User userMerge = em.merge(user);
             final Album album = getAlbum(albumId, em);
             final AlbumUser albumUser = getAlbumUser(album, userMerge, em);
-            if (userMerge.getInbox() != album) {
+            if (userMerge.getInbox() == album) {
                 throw new AlbumNotFoundException("Album id : " + albumId + " not found");
             } else {
                 return true;
