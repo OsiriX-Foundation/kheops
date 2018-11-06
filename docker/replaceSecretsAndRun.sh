344524c488b3ca8bb28072bf95495a6388068ba2
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2018-11-06 17:08:44 +0100

    Change env var

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -7,7 +7,7 @@ do
   sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
 done
 
-sed -i "s|\${kheops_postgresql_user}|$KHEOPS_POSTGRESQL_USER|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_postgresql_user}|$AUTHDB_USER|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_postgresql_url}|$KHEOPS_POSTGRESQL_URL|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_pacs_url}|$KHEOPS_PACS_URL|" ${REPLACE_FILE_PATH}
 
