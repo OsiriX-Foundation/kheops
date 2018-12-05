3dd69c73406fc5ecb9d3339d66f5b657b23d0c56
Joël Spaltenstein <spalte@naturalimage.ch>
2018-12-05 11:57:14 +0100

    add root URL and fix post secret

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
+for f in ${SECRET_FILE_PATH}
+do
+  filename=$(basename "$f")
+  value=$(cat ${f})
+  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
+done
+
 sed -i "s|\${kheops_pacs_url}|$KHEOPS_PACS_URL|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_authorization_url}|$KHEOPS_AUTHORIZATION_URL|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_root_url}|$KHEOPS_ROOT_URL|" ${REPLACE_FILE_PATH}
 
 catalina.sh run;
