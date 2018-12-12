dc613a4d30d44965bc976980a6710dc6db595c1a
gerome12 <gerome.pasquier@gmail.com>
2018-12-12 15:12:24 +0100

    Update deploy_no_tls.sh

--- docker/deploy_no_tls.sh
+++ docker/deploy_no_tls.sh
@@ -2,5 +2,5 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build ./docker/ -t osirixfoundation/kheopsUI:no-tls
-docker push osirixfoundation/kheopsUI:no-tls
+docker build ./docker/ -t osirixfoundation/kheops-ui:no-tls
+docker push osirixfoundation/kheops-ui:no-tls
