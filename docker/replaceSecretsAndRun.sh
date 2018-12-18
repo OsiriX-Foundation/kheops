e5ac75fd5a460c160e4f49e98e9fdf5ad8708dd4
Joël Spaltenstein <spalte@naturalimage.ch>
2018-12-18 10:46:25 +0100

    new env var names

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sed -i "s|\${kheops_auth_url}|$KHEOPS_AUTHORIZATION_URL|" ${REPLACE_FILE_PATH}
-sed -i "s|\${kheops_pacs_url}|$KHEOPS_PACS_URL|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_auth_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT$KHEOPS_AUTHORIZATION_PATH|" ${REPLACE_FILE_PATH}
+sed -i "s|\${kheops_pacs_url}|http://$KHEOPS_PACS_PEP_HOST:$KHEOPS_PACS_PEP_PORT|" ${REPLACE_FILE_PATH}
 
 catalina.sh run;
