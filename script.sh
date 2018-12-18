bafaaf3bf9540fa86daa9a228e19dd9c8f18d97f
gerome12 <gerome.pasquier@gmail.com>
2018-12-18 15:30:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sed -i "s|\${root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" /etc/nginx/conf
 
 sed -i "s|\${DICOMWebProxy_url}|http://$KHEOPS_DICOMWEB_PROXY_HOST:$KHEOPS_DICOMWEB_PROXY_PORT|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsAuthorization_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsAuthorizationProxy_url}|http://$KHEOPS_PEP_HOST:$KHEOPS_PEP_PORT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorizationProxy_url}|http://$KHEOPS_PACS_PEP_HOST:$KHEOPS_PACS_PEP_PORT|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsZipper_url}|http://$KHEOPS_ZIPPER_HOST:$KHEOPS_ZIPPER_PORT|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsWebUI_url}|http://$KHEOPS_VIEWER_HOST:$KHEOPS_VIEWER_PORT|" /etc/nginx/conf.d/kheops.conf
 
