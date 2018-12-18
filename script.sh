c10fb9ebf57553e7964e51cbf2dbf58a39b87dda
gerome12 <gerome.pasquier@gmail.com>
2018-12-18 11:06:54 +0100

    new env var

--- script.sh
+++ script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 chmod a+w /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/kheops.conf
 
-sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_NGINX_URL_DICOMWEB_PROXY|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_NGINX_URL_AUTHORIZATION|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsAuthorizationProxy_url}|$KHEOPS_NGINX_URL_AUTHORIZATION_PROXY|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsZipper_url}|$KHEOPS_NGINX_URL_ZIPPER|" /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${kheopsWebUI_url}|$KHEOPS_NGINX_URL_VIEWER|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_DICOMWEB_PROXY_HOST:$KHEOPS_DICOMWEB_PROXY_PORT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorization_url}|$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsAuthorizationProxy_url}|$KHEOPS_PEP_HOST:$KHEOPS_PEP_PORT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsZipper_url}|$KHEOPS_ZIPPER_HOST:$KHEOPS_ZIPPER_PORT|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${kheopsWebUI_url}|$KHEOPS_VIEWER_HOST:$KHEOPS_VIEWER_PORT|" /etc/nginx/conf.d/kheops.conf
 
-https="https://"
-server_name=${KHEOPS_ROOT_URL/$https/}
 
-sed -i "s|\${server_name}|$server_name|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${server_name}|$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/kheops.conf
 
 nginx-debug -g 'daemon off;'
