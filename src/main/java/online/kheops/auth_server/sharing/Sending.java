aa64f5ead04a81032de8914109a60502756d459c
gerome12 <gerome.pasquier@gmail.com>
2019-01-04 13:53:27 +0100

    add debug logs

--- src/main/java/online/kheops/auth_server/sharing/Sending.java
+++ src/main/java/online/kheops/auth_server/sharing/Sending.java
@@ -159,6 +159,7 @@ public class Sending {
     public static void putSeriesInAlbum(long callingUserPk, String albumId, String studyInstanceUID, String seriesInstanceUID)
             throws UserNotFoundException, AlbumNotFoundException {
 
+        LOG.info(() -> "DEBUG:inside put series in album id:"+albumId);
         final EntityManager em = EntityManagerListener.createEntityManager();
         final EntityTransaction tx = em.getTransaction();
 
