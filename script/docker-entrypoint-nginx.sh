d1ae5b00c891379b73211760254ec05e8466bf2f
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-02-07 16:36:33 +0100

    Update environment variables name

--- script/docker-entrypoint-nginx.sh
+++ script/docker-entrypoint-nginx.sh
@@ -3,12 +3,12 @@
 DIR=/usr/share/nginx/html/js/
 FILENAME=$(find /usr/share/nginx/html/js/ -name 'app.*.js')
 
-sed -i "s|\${kheops_viewer_title}|$KHEOPS_VIEWER_TITLE|" $FILENAME
-sed -i "s|\${kheops_keycloak_url}|$KHEOPS_KEYCLOAK_URL|" $FILENAME
+sed -i "s|\${kheops_ui_title}|$KHEOPS_UI_TITLE|" $FILENAME
+sed -i "s|\${kheops_keycloak_uri}|$KHEOPS_KEYCLOAK_URI|" $FILENAME
 sed -i "s|\${kheops_keycloak_realms}|$KHEOPS_KEYCLOAK_REALMS|" $FILENAME
-sed -i "s|\${kheops_keycloak_viewer_clientid}|$KHEOPS_KEYCLOAK_VIEWER_CLIENTID|" $FILENAME
-sed -i "s|\${kheops_url_api}|$KHEOPS_URL_API|" $FILENAME
-sed -i "s|\${kheops_url_viewer}|$KHEOPS_URL_VIEWER|" $FILENAME
+sed -i "s|\${kheops_ui_keycloak_clientid}|$KHEOPS_UI_KEYCLOAK_CLIENTID|" $FILENAME
+sed -i "s|\${kheops_api_url}|$KHEOPS_API_URL|" $FILENAME
+sed -i "s|\${kheops_viewer_url}|$KHEOPS_VIEWER_URL|" $FILENAME
 
 chmod a+w /etc/nginx/conf.d/ui.conf
 sed -i "s|\${server_name}|$SERVER_NAME|" /etc/nginx/conf.d/ui.conf
