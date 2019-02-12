98821a55701ba97e2671fd635e987a4e687dae46
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-02-12 16:40:40 +0100

    Update docker entrypoint

--- script/docker-entrypoint-nginx.sh
+++ script/docker-entrypoint-nginx.sh
@@ -2,12 +2,14 @@
 
 DIR=/usr/share/nginx/html/js/
 FILENAME=$(find /usr/share/nginx/html/js/ -name 'app.*.js')
-
 sed -i "s|\${kheops_ui_title}|$KHEOPS_UI_TITLE|" $FILENAME
 sed -i "s|\${kheops_keycloak_uri}|$KHEOPS_KEYCLOAK_URI|" $FILENAME
 sed -i "s|\${kheops_keycloak_realms}|$KHEOPS_KEYCLOAK_REALMS|" $FILENAME
 sed -i "s|\${kheops_ui_keycloak_clientid}|$KHEOPS_UI_KEYCLOAK_CLIENTID|" $FILENAME
-sed -i "s|\${kheops_api_url}|$KHEOPS_API_URL|" $FILENAME
+while [ $(tr ' ' '\n' < $FILENAME | grep {kheops_api_url} | wc -l) -gt 0 ]
+do
+    sed -i "s|\${kheops_api_url}|$KHEOPS_API_URL|" $FILENAME
+done
 sed -i "s|\${kheops_viewer_url}|$KHEOPS_VIEWER_URL|" $FILENAME
 
 chmod a+w /etc/nginx/conf.d/ui.conf
