83ee5f6d80a456d90d5ac530594b17e946b302bf
gerome12 <gerome.pasquier@gmail.com>
2019-02-12 15:13:45 +0100

    event response add fav add series and study UID

--- src/main/java/online/kheops/auth_server/event/EventResponse.java
+++ src/main/java/online/kheops/auth_server/event/EventResponse.java
@@ -64,11 +64,16 @@ public class EventResponse {
             response.targetName = mutation.getToUser().getEmail();
         }
         if (mutation.getMutationType().compareTo(Events.MutationType.IMPORT_SERIES.toString()) == 0 ||
-                mutation.getMutationType().compareTo(Events.MutationType.REMOVE_SERIES.toString()) == 0 ||
-                mutation.getMutationType().compareTo(Events.MutationType.ADD_FAV.toString()) == 0 ||
-                mutation.getMutationType().compareTo(Events.MutationType.REMOVE_FAV.toString()) == 0) {
+                mutation.getMutationType().compareTo(Events.MutationType.REMOVE_SERIES.toString()) == 0 ) {
             response.series = mutation.getSeries().getSeriesInstanceUID();
             response.study = mutation.getSeries().getStudy().getStudyInstanceUID();
+        }
+        if (mutation.getMutationType().compareTo(Events.MutationType.ADD_FAV.toString()) == 0 ||
+                mutation.getMutationType().compareTo(Events.MutationType.REMOVE_FAV.toString()) == 0) {
+            if(mutation.getSeries() != null) {
+                response.series = mutation.getSeries().getSeriesInstanceUID();
+            }
+            response.study = mutation.getStudy().getStudyInstanceUID();
         }
         if (mutation.getMutationType().compareTo(Events.MutationType.IMPORT_STUDY.toString()) == 0 ||
                 mutation.getMutationType().compareTo(Events.MutationType.REMOVE_STUDY.toString()) == 0 ) {
