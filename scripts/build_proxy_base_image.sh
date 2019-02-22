86477de2948d7795b28554e81ea00ad4b9a26899
gerome12 <gerome.pasquier@gmail.com>
2019-02-22 16:15:21 +0100

    Update build_proxy_base_image.sh

--- scripts/build_proxy_base_image.sh
+++ scripts/build_proxy_base_image.sh
@@ -12,5 +12,5 @@ echo -e "${cyan}Required Dockerfile SHA1:${no_color} $dockerfile_sha1"
 
 echo -e "${cyan}Building base proxy image${no_color}"
 
-docker build --build-arg VCS_REF=`git rev-parse --short HEAD` -t="osirixfoundation/pacs-authorization-proxy:$1" --force-rm $proxy_base_dir
+docker build --build-arg VCS_REF=`git rev-parse --short HEAD` -t="osirixfoundation/pacs-authorization-proxy:$BRANCH" --force-rm $proxy_base_dir
 
