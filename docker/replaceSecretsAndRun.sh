c9d8a67be49ae5b9688a1d2b82729a09a112cfbd
gerome12 <gerome.pasquier@gmail.com>
2019-02-25 09:41:29 +0100

    verify secrets

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -27,7 +27,7 @@ for f in ${SECRET_FILE_PATH}/*
 do
   filename=$(basename "$f")
   value=$(cat ${f})
-  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}/*
+  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
 done
 
 sed -i "s|\${kheops_postgresql_user}|$KHEOPS_AUTHDB_USER|" ${REPLACE_FILE_PATH}
