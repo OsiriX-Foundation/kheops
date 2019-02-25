95bd316277b257eb51ebd148a7d6081c17e04c45
gerome12 <gerome.pasquier@gmail.com>
2019-02-25 13:32:30 +0100

    verify env var and secret

--- script.sh
+++ script.sh
@@ -1,50 +1,75 @@
 #!/bin/bash
 
-missing_env_var=false
+missing_env_var_secret=false
 
+#Verify secrets
+if ! [ -f ${SECRET_FILE_PATH}/privkey1.pem ]; then
+    echo "Missing privkey1.pem secret"
+    missing_env_var_secret=true
+fi
+if ! [ -f ${SECRET_FILE_PATH}/fullchain1.pem ]; then
+    echo "Missing fullchain1.pem secret"
+    missing_env_var_secret=true
+fi
+
+
+#Verify environment variables
 if [[ -z $KHEOPS_ROOT_SCHEME ]]; then
   echo "Missing KHEOPS_ROOT_SCHEME environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_ROOT_HOST ]]; then
   echo "Missing KHEOPS_ROOT_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_DICOMWEB_PROXY_HOST ]]; then
   echo "Missing KHEOPS_DICOMWEB_PROXY_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_DICOMWEB_PROXY_PORT ]]; then
   echo "Missing KHEOPS_DICOMWEB_PROXY_PORT environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_AUTHORIZATION_HOST ]]; then
   echo "Missing KHEOPS_AUTHORIZATION_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_AUTHORIZATION_PORT ]]; then
   echo "Missing KHEOPS_AUTHORIZATION_PORT environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_PACS_PEP_HOST ]]; then
   echo "Missing KHEOPS_PACS_PEP_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_PACS_PEP_PORT ]]; then
   echo "Missing KHEOPS_PACS_PEP_PORT environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_ZIPPER_HOST ]]; then
   echo "Missing KHEOPS_ZIPPER_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_ZIPPER_PORT ]]; then
   echo "Missing KHEOPS_ZIPPER_PORT environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_UI_HOST ]]; then
   echo "Missing KHEOPS_UI_HOST environment variable"
+  missing_env_var_secret=true
 fi
 if [[ -z $KHEOPS_UI_PORT ]]; then
   echo "Missing KHEOPS_UI_PORT environment variable"
+  missing_env_var_secret=true
 fi
 
 
-if [[ missing_env_var=true ]]; then
+#if missing env var or secret => exit
+if [[ missing_env_var_secret=true ]]; then
   exit 1
 fi
 
-
+#get env var
 chmod a+w /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/kheops.conf
 
