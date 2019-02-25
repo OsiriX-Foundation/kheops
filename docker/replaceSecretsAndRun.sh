8eeb0b12ec56a92ffbc60701e1a2ab5063ff3d89
gerome12 <gerome.pasquier@gmail.com>
2019-02-25 10:40:19 +0100

    remove verify env var

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -30,43 +30,6 @@ do
 done
 
 
-if [[ -z "$KHEOPS_AUTHDB_USER" ]]; then
-    echo "Missing KHEOPS_AUTHDB_USER environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_AUTHDB_URL" ]]; then
-    echo "Missing KHEOPS_AUTHDB_URL environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_AUTHDB_NAME" ]]; then
-    echo "Missing KHEOPS_AUTHDB_NAME environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_PACS_PEP_HOST" ]]; then
-    echo "Missing KHEOPS_PACS_PEP_HOST environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_PACS_PEP_PORT" ]]; then
-    echo "Missing KHEOPS_PACS_PEP_PORT environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_KEYCLOAK_URI" ]]; then
-    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_KEYCLOAK_CLIENTID" ]]; then
-    echo "Missing KHEOPS_KEYCLOAK_CLIENTID environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_KEYCLOAK_USER" ]]; then
-    echo "Missing KHEOPS_KEYCLOAK_USER environment variable"
-    exit 1
-fi
-if [[ -z "$KHEOPS_KEYCLOAK_REALMS" ]]; then
-    echo "Missing KHEOPS_KEYCLOAK_REALMS environment variable"
-    exit 1
-fi
-
 
 sed -i "s|\${kheops_postgresql_user}|$KHEOPS_AUTHDB_USER|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_postgresql_url}|$KHEOPS_AUTHDB_URL/$KHEOPS_AUTHDB_NAME|" ${REPLACE_FILE_PATH}
