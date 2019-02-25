28877744b36311c0b8f78e2cc660b158b568276d
gerome12 <gerome.pasquier@gmail.com>
2019-02-25 11:22:38 +0100

    verify all secret ans all env var

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -1,26 +1,72 @@
 #! /bin/sh
 
+missing_env_var_secret=false
 
 if ! [ -f ${SECRET_FILE_PATH}/kheops_authdb_pass ]; then
     echo "Missing kheops_authdb_pass secret"
-    exit 1
+    missing_env_var_secret=true
 fi
 if ! [ -f ${SECRET_FILE_PATH}/kheops_superuser_hmasecret ]; then
     echo "Missing kheops_superuser_hmasecret secret"
-    exit 1
+    missing_env_var_secret=true
 fi
 if ! [ -f ${SECRET_FILE_PATH}/kheops_auth_hmasecret ]; then
     echo "Missing kheops kheops_auth_hmasecret secret"
-    exit 1
+    missing_env_var_secret=true
 fi
 if ! [ -f ${SECRET_FILE_PATH}/kheops_keycloak_clientsecret ]; then
     echo "Missing kheops_keycloak_clientsecret secret"
-    exit 1
+    missing_env_var_secret=true
 fi
 if ! [ -f ${SECRET_FILE_PATH}/kheops_keycloak_password ]; then
     echo "Missing kheops_keycloak_password secret"
+    missing_env_var_secret=true
+fi
+
+
+
+if [ -z "$KHEOPS_AUTHDB_USER" ]; then
+    echo "Missing KHEOPS_AUTHDB_USER environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_AUTHDB_URL" ]; then
+    echo "Missing KHEOPS_AUTHDB_URL environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_AUTHDB_NAME" ]; then
+    echo "Missing KHEOPS_AUTHDB_NAME environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_PACS_PEP_HOST" ]; then
+    echo "Missing KHEOPS_PACS_PEP_HOST environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_PACS_PEP_PORT" ]; then
+    echo "Missing KHEOPS_PACS_PEP_PORT environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_KEYCLOAK_URI" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_KEYCLOAK_CLIENTID" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_CLIENTID environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_KEYCLOAK_USER" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_USER environment variable"
+    missing_env_var_secret=true
+fi
+if [ -z "$KHEOPS_KEYCLOAK_REALMS" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_REALMS environment variable"
+    missing_env_var_secret=true
+fi
+
+
+if [ "missing_env_var_secret" = true ]; then
     exit 1
 fi
+
 
 for f in ${SECRET_FILE_PATH}/*
 do
@@ -29,48 +75,6 @@ do
   sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
 done
 
-missing_env_var=false
-
-if [ -z "$KHEOPS_AUTHDB_USER" ]; then
-    echo "Missing KHEOPS_AUTHDB_USER environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_AUTHDB_URL" ]; then
-    echo "Missing KHEOPS_AUTHDB_URL environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_AUTHDB_NAME" ]; then
-    echo "Missing KHEOPS_AUTHDB_NAME environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_PACS_PEP_HOST" ]; then
-    echo "Missing KHEOPS_PACS_PEP_HOST environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_PACS_PEP_PORT" ]; then
-    echo "Missing KHEOPS_PACS_PEP_PORT environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_KEYCLOAK_URI" ]; then
-    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_KEYCLOAK_CLIENTID" ]; then
-    echo "Missing KHEOPS_KEYCLOAK_CLIENTID environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_KEYCLOAK_USER" ]; then
-    echo "Missing KHEOPS_KEYCLOAK_USER environment variable"
-    missing_env_var=true
-fi
-if [ -z "$KHEOPS_KEYCLOAK_REALMS" ]; then
-    echo "Missing KHEOPS_KEYCLOAK_REALMS environment variable"
-    missing_env_var=true
-fi
-
-if [ "$missing_env_var" = true ]; then
-    exit 1
-fi
 
 sed -i "s|\${kheops_postgresql_user}|$KHEOPS_AUTHDB_USER|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_postgresql_url}|$KHEOPS_AUTHDB_URL/$KHEOPS_AUTHDB_NAME|" ${REPLACE_FILE_PATH}
