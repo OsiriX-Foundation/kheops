592ab1eede6ae89d81163b8935fc3165b67a8052
gerome12 <gerome.pasquier@gmail.com>
2019-02-25 09:39:02 +0100

    verify in secrets are present

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -23,11 +23,11 @@ if ! [ -f ${SECRET_FILE_PATH}/kheops_keycloak_password ]; then
 fi
 
 
-for f in ${SECRET_FILE_PATH}
+for f in ${SECRET_FILE_PATH}/*
 do
   filename=$(basename "$f")
   value=$(cat ${f})
-  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}
+  sed -i "s|\${$filename}|$value|" ${REPLACE_FILE_PATH}/*
 done
 
 sed -i "s|\${kheops_postgresql_user}|$KHEOPS_AUTHDB_USER|" ${REPLACE_FILE_PATH}
