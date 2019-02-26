ced88a1911e705716b306fbc36e9b4845d8a8ddf
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-02-26 08:58:38 +0100

    check env var

--- script/docker-entrypoint-nginx.sh
+++ script/docker-entrypoint-nginx.sh
@@ -2,17 +2,62 @@
 
 DIR=/usr/share/nginx/html/js/
 FILENAME=$(find /usr/share/nginx/html/js/ -name 'app.*.js')
-sed -i "s|\${kheops_ui_title}|$KHEOPS_UI_TITLE|" $FILENAME
-sed -i "s|\${kheops_keycloak_uri}|$KHEOPS_KEYCLOAK_URI|" $FILENAME
-sed -i "s|\${kheops_keycloak_realms}|$KHEOPS_KEYCLOAK_REALMS|" $FILENAME
-sed -i "s|\${kheops_ui_keycloak_clientid}|$KHEOPS_UI_KEYCLOAK_CLIENTID|" $FILENAME
-while [ $(tr ' ' '\n' < $FILENAME | grep {kheops_api_url} | wc -l) -gt 0 ]
-do
-    sed -i "s|\${kheops_api_url}|$KHEOPS_API_URL|" $FILENAME
-done
-sed -i "s|\${kheops_viewer_url}|$KHEOPS_VIEWER_URL|" $FILENAME
+
+missing_env_var_secret=false
+
+#Verify environment variables
+if [ -z "$KHEOPS_UI_TITLE" ]; then
+    echo "Missing KHEOPS_UI_TITLE environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_KEYCLOAK_URI" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_KEYCLOAK_REALMS" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_UI_KEYCLOAK_CLIENTID" ]; then
+    echo "Missing KHEOPS_KEYCLOAK_URI environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_VIEWER_URL" ]; then
+    echo "Missing KHEOPS_VIEWER_URL environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_ROOT_SCHEME" ]; then
+    echo "Missing KHEOPS_ROOT_SCHEME environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_ROOT_HOST" ]; then
+    echo "Missing KHEOPS_ROOT_HOST environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ -z "$KHEOPS_API_PATH" ]; then
+    echo "Missing KHEOPS_API_PATH environment variable"
+    missing_env_var_secret=true
+fi
+
+if [ "$missing_env_var_secret" = true ]; then
+    exit 1
+fi
+
+sed -i "s|\${kheops_ui_title}|$KHEOPS_UI_TITLE|g" $FILENAME
+sed -i "s|\${kheops_keycloak_uri}|$KHEOPS_KEYCLOAK_URI|g" $FILENAME
+sed -i "s|\${kheops_keycloak_realms}|$KHEOPS_KEYCLOAK_REALMS|g" $FILENAME
+sed -i "s|\${kheops_ui_keycloak_clientid}|$KHEOPS_UI_KEYCLOAK_CLIENTID|g" $FILENAME
+api="${KHEOPS_ROOT_SCHEME}://${KHEOPS_ROOT_HOST}${KHEOPS_API_PATH}"
+sed -i "s|\${kheops_api_url}|$api|g" $FILENAME
+sed -i "s|\${kheops_viewer_url}|$KHEOPS_VIEWER_URL|g" $FILENAME
 
 chmod a+w /etc/nginx/conf.d/ui.conf
-sed -i "s|\${server_name}|$SERVER_NAME|" /etc/nginx/conf.d/ui.conf
 
 nginx-debug -g 'daemon off;'
