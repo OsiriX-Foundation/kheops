889ad9fa5256535a95c251277ee415e2d715f46c
gerome12 <gerome.pasquier@gmail.com>
2019-02-26 15:41:47 +0100

    capability filter last_used

--- src/main/java/online/kheops/auth_server/capability/CapabilitiesResponse.java
+++ src/main/java/online/kheops/auth_server/capability/CapabilitiesResponse.java
@@ -1,6 +1,5 @@
 package online.kheops.auth_server.capability;
 
-import online.kheops.auth_server.album.AlbumResponse;
 import online.kheops.auth_server.entity.Capability;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -32,8 +31,8 @@ public class CapabilitiesResponse {
         String issuedAt;
         @XmlElement(name = "not_before_time")
         String notBeforeTime;
-        @XmlElement(name = "last_use")
-        String lastUse;
+        @XmlElement(name = "last_used")
+        String lastUsed;
         @XmlElement(name = "expiration_time")
         String expirationTime;
         @XmlElement(name = "revoke_time")
@@ -71,7 +70,7 @@ public class CapabilitiesResponse {
             response.title = capability.getTitle();
             response.issuedAt = ZonedDateTime.of(capability.getIssuedAtTime(), ZoneOffset.UTC).toString();
             if(capability.getLastUse() != null) {
-                response.lastUse = ZonedDateTime.of(capability.getLastUse(), ZoneOffset.UTC).toString();
+                response.lastUsed = ZonedDateTime.of(capability.getLastUse(), ZoneOffset.UTC).toString();
             }
         }
 
