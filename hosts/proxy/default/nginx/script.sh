368249891af6f7b48c2b7a95021a4543b496942d
gerome12 <gerome.pasquier@gmail.com>
2019-03-08 15:48:39 +0100

    Update script.sh

--- hosts/proxy/default/nginx/script.sh
+++ hosts/proxy/default/nginx/script.sh
@@ -61,4 +61,76 @@ export JWT_SECRET=$kheops_auth_hmasecret
 export JWT_POST_SECRET=$kheops_auth_hmasecret_post
 
 
+echo "Ending setup NGINX secrets and env var"
+
+#######################################################################################
+#ELASTIC SEARCH
+
+
+missing_env_var_secret=false
+
+#Verify secrets
+if ! [ -f ${SECRET_FILE_PATH}/elastic_cloud_id ]; then
+    echo "Missing elastic_cloud_id secret"
+    missing_env_var_secret=true
+else
+   echo -e "secret elastic_cloud_id \e[92mOK\e[0m"
+fi
+
+if ! [ -f ${SECRET_FILE_PATH}/elastic_cloud_auth ]; then
+    echo "Missing elastic_cloud_auth secret"
+    missing_env_var_secret=true
+else
+   echo -e "secret elastic_cloud_authm \e[92mOK\e[0m"
+fi
+
+
+#get secrets and verify content
+for f in ${SECRET_FILE_PATH}/*
+do
+  filename=$(basename "$f")
+  value=$(cat ${f})
+  sed -i "s|\${$filename}|$value|" /etc/metricbeat/metricbeat.yml
+  sed -i "s|\${$filename}|$value|" /etc/filebeat/filebeat.yml
+done
+
+if [[ -z $KHEOPS_REVERSE_PROXY_ELASTIC_NAME ]]; then
+  echo "Missing KHEOPS_REVERSE_PROXY_ELASTIC_NAME environment variable"
+  missing_env_var_secret=true
+else
+   echo -e "environment variable KHEOPS_REVERSE_PROXY_ELASTIC_NAME \e[92mOK\e[0m"
+   sed -i "s|\${elastic_name}|$KHEOPS_REVERSE_PROXY_ELASTIC_NAME|" /etc/metricbeat/metricbeat.yml
+   sed -i "s|\${elastic_name}|$KHEOPS_REVERSE_PROXY_ELASTIC_NAME|" /etc/filebeat/filebeat.yml
+fi
+if [[ -z $KHEOPS_REVERSE_PROXY_ELASTIC_TAGS ]]; then
+  echo "Missing KHEOPS_REVERSE_PROXY_ELASTIC_TAGS environment variable"
+  missing_env_var_secret=true
+else
+   echo -e "environment variable KHEOPS_REVERSE_PROXY_ELASTIC_TAGS \e[92mOK\e[0m"
+   sed -i "s|\${elastic_tags}|$KHEOPS_REVERSE_PROXY_ELASTIC_TAGS|" /etc/metricbeat/metricbeat.yml
+   sed -i "s|\${elastic_tags}|$KHEOPS_REVERSE_PROXY_ELASTIC_TAGS|" /etc/filebeat/filebeat.yml
+fi
+
+#if missing env var or secret => exit
+if [[ $missing_env_var_secret = true ]]; then
+  exit 1
+else
+   echo -e "all elastic secrets and all env var \e[92mOK\e[0m"
+fi
+
+metricbeat modules enable nginx
+filebeat modules enable nginx
+metricbeat modules disabled system
+filebeat modules disabled system
+
+service filebeat start
+service metricbeat start
+
+echo "Ending setup METRICBEAT and FILEBEAT"
+
+#######################################################################################
+
+
+
+
 nginx -g 'daemon off; error_log /dev/stderr info;'
