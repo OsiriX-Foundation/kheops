d0753cbabb92d7d932231f21e01b4f7cffb05dc6
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-03-12 09:47:15 +0100

    check if Date is valid

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -3,22 +3,21 @@ import moment from 'moment'
 
 Vue.filter('formatDate', value => {
 	if (value) {
-		return moment(String(value)).format('MMM Do YYYY')
+		return moment(String(value)).isValid() ? moment(String(value)).format('MMM Do YYYY') : 'Invalid Date'
 	}
 	return value
 })
 
 Vue.filter('formatDateTime', value => {
 	if (value) {
-		return moment(String(value)).format('MMM Do YYYY HH:mm')
+		return moment(String(value)).isValid() ? moment(String(value)).format('MMM Do YYYY') : 'Invalid Date'
 	}
 	return value
 })
 
-
 Vue.filter('formatTime', value => {
 	if (value) {
-		return moment(String(value)).format('HH:mm')
+		return moment(String(value)).isValid() ? moment(String(value)).format('MMM Do YYYY') : 'Invalid Date'
 	}
 	return value
 })
