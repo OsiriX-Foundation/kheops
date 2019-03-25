e9436cedc5ca5916f62f735b3bc2b63b3e2d939d
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-03-25 14:06:35 +0100

    still generate 502 and need to be improve

--- src/components/study/InputImportStudy.vue
+++ src/components/study/InputImportStudy.vue
@@ -40,22 +40,22 @@
             <div
               class="col"
             >
-              {{ manageFiles[index].path }}
+              {{ file.path }}
             </div>
             <div
               class="col-sm-auto"
             >
               <span
-                v-if="manageFiles[index].sendFiles && !manageFiles[index].done"
+                v-if="file.state.sendFiles && !file.state.done"
               >
                 <clip-loader
-                  :loading="manageFiles[index].sendFiles"
+                  :loading="file.state.sendFiles"
                   :color="colorSpinner"
                   :size="sizeSpinner"
                 />
               </span>
               <span
-                v-else-if="manageFiles[index].done"
+                v-else-if="file.state.done"
               >
                 <v-icon
                   color="green"
@@ -63,20 +63,36 @@
                   name="check"
                 />
               </span>
-              <button
+              <span
                 v-else
-                type="button"
-                class="btn btn-link btn-sm text-center"
-                @click="removeFile(index)"
               >
-                <span>
-                  <v-icon
-                    color="red"
-                    class="align-middle"
-                    name="trash"
-                  />
-                </span>
-              </button>
+                <button
+                  v-if="file.state.err"
+                  type="button"
+                  class="btn btn-link btn-sm text-center"
+                >
+                  <span>
+                    <error-icon
+                      :height="'22'"
+                      :width="'22'"
+                      color="red"
+                    />
+                  </span>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-link btn-sm text-center"
+                  @click="removeFileName(file.name)"
+                >
+                  <span>
+                    <v-icon
+                      color="red"
+                      class="align-middle"
+                      name="trash"
+                    />
+                  </span>
+                </button>
+              </span>
             </div>
           </div>
         </div>
@@ -102,22 +118,28 @@
 
 <script>
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
-import axios from 'axios'
+import { HTTP } from '@/router/http'
+import ErrorIcon from '@/components/kheopsSVG/ErrorIcon.vue'
 
 export default {
 	name: 'DragAndDrop',
-	components: { ClipLoader },
+	components: { ClipLoader, ErrorIcon },
 	data () {
 		return {
 			dragAndDropCapable: false,
 			files: [],
-			manageFiles: [],
 			colorSpinner: 'white',
 			sizeSpinner: '24px',
-			hover: false
+			hover: false,
+			maxsize: 10e6
 		}
 	},
 	computed: {
+		totalSizeFiles () {
+			return this.files.reduce(function (total, currentValue) {
+				return total + currentValue.content.size
+			}, 0)
+		}
 	},
 	watch: {
 	},
@@ -167,18 +189,23 @@ export default {
 			var div = document.createElement('div')
 			return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window
 		},
-		loadFile (file, path) {
-			this.files.push(file)
-			this.manageFiles.push({
-				path: path,
-				done: false,
-				sendFiles: false
-			})
+		loadFile (file, path, name) {
+			let objFile = {
+				'content': file,
+				'state': {
+					done: false,
+					sendFiles: false,
+					err: false
+				},
+				'path': path,
+				'name': name
+			}
+			this.files.push(objFile)
 		},
 		inputLoadFiles () {
 			let files = this.$refs.inputfiles.files
 			for (let i = 0; i < files.length; i++) {
-				this.loadFile(files[i], files[i].webkitRelativePath ? files[i].webkitRelativePath : files[i].name)
+				this.loadFile(files[i], files[i].webkitRelativePath ? files[i].webkitRelativePath : files[i].name, files[i].name)
 			}
 		},
 		loadDir (dir, parentDir) {
@@ -187,7 +214,7 @@ export default {
 				this.readDir(directoryReader)
 			} else {
 				dir.file(function (file) {
-					this.loadFile(file, `${parentDir}`)
+					this.loadFile(file, `${parentDir}`, file.name)
 				}.bind(this))
 			}
 		},
@@ -201,24 +228,91 @@ export default {
 				}
 			}.bind(this))
 		},
-		removeFile (index) {
-			this.files.splice(index, 1)
-			this.manageFiles.splice(index, 1)
-		},
 		sendFiles () {
-			this.files.forEach((file, index) => {
-				if (!this.manageFiles[index].done) {
-					this.sendFile(file, index)
+			const config = {
+				headers: {
+					'Accept': 'application/dicom+json'
+				}
+			}
+			if (this.maxsize > this.totalSizeFiles) {
+				let formData = this.createFormData(this.files)
+				this.sendFormData(formData, config)
+			} else {
+				this.sendBySize(this.files, config)
+			}
+		},
+		sendBySize (files, config) {
+			let size = 0
+			let tmpIndex = 0
+			const copyFiles = files
+			copyFiles.forEach((file, index) => {
+				size += file.content.size
+				if (size > this.maxsize) {
+					let formData = this.createFormData(files.slice(tmpIndex, index))
+
+					this.sendFormData(formData, config)
+					tmpIndex = index
+					size = 0
 				}
 			})
+			if (tmpIndex < copyFiles.length) {
+				let formData = this.createFormData(copyFiles.slice(tmpIndex, copyFiles.length))
+				this.sendFormData(formData, config)
+			}
 		},
-		sendFile (file, index) {
+		createFormData (files) {
 			let formData = new FormData()
-			this.manageFiles[index].sendFiles = true
-			formData.append(file.name, file)
-			setTimeout(() => {
-				this.manageFiles[index].done = true
-			}, Math.floor(Math.random() * 3000) + 2000)
+			files.forEach((file) => {
+				file.state.sendFiles = true
+				if (!file.state.done) {
+					formData.append(file.content.name, file.content)
+				}
+			})
+			return formData
+		},
+		sendFormData (formData, config) {
+			HTTP.post('/studies', formData, config).then(res => {
+				// TODO: Toutes les données sont passées
+				if (res.status === 200) {
+					formData.forEach((val) => {
+						this.removeFileName(val.name)
+					})
+				}
+				// TODO: Quelques données sont passées
+				if (res.status === 202) {
+					this.errDicom(formData, res.data, '0008119A', '00041500')
+					this.errDicom(formData, res, '00081198', '00081150')
+				}
+			}).catch(res => {
+				this.errDicom(formData, res, '0008119A', '00041500')
+				this.errDicom(formData, res, '00081198', '00081150')
+			})
+		},
+		errDicom (formData, res, iderr, idvalue) {
+			if (iderr in res) {
+				let err = this.dicom2array(res[iderr].Value, idvalue)
+
+				formData.forEach((val) => {
+					if (err.indexOf(val.name) === -1) {
+						this.removeFileName(val.name)
+					}
+				})
+				this.files.forEach((val) => {
+					val.state.sendFiles = false
+					val.state.err = true
+				})
+			}
+		},
+		dicom2array (dicom, id) {
+			let tab = []
+			dicom.forEach(x => {
+				tab.push(x[id].Value[0])
+			})
+			return tab
+		},
+		removeFileName (filename) {
+			let index = this.files.findIndex(x => x.name === filename)
+			this.files.splice(index, 1)
 		}
 	}
 }
