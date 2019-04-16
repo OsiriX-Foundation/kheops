861a7db6b3385e736c6a1f7395d1d823cc529da2
gerome12 <gerome.pasquier@gmail.com>
2019-04-16 15:53:40 +0200

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -2,6 +2,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 mv build/libs/KheopsDICOMwebProxy.war docker/KheopsDICOMwebProxy.war
+mv filebeat docker
 
 docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheops-dicomweb-proxy:$BRANCH
 
