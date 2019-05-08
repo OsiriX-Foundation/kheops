c001b46f147cacfbb7ee2ca3208f2586b26e092f
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-05-08 15:33:13 +0200

    prepare providers store

--- src/store/modules/providers.js
+++ src/store/modules/providers.js
@@ -2,7 +2,8 @@ import { HTTP } from '@/router/http'
 
 // initial state
 const state = {
-	providers: []
+	providers: [],
+	provider: {}
 }
 
 const getters = {
@@ -10,13 +11,22 @@ const getters = {
 }
 
 const actions = {
+	getProviders ({ commit }, params) {
+		commit('SET_PROVIDERS', params)
+	},
+	getProvider ({ commit }, params) {
+		commit('SET_PROVIDER', params)
+	},
+	updateProvider ({ commit }, params) {
+		commit('UPDATE_PROVIDER', params)
+	},
 	postProvider ({ dispatch }, params) {
 		const albumID = params.albumID
-		let query = ''
+		let queries = ''
 		for (var key in params.query) {
-			query += `${key}=${params.query[key]}&`
+			queries += `${key}=${params.query[key]}&`
 		}
-		return HTTP.post('/albums/' + albumID + '/reportprovider', query, { headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' } }).then(res => {
+		return HTTP.post('/albums/' + albumID + '/reportprovider', queries, { headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' } }).then(res => {
 			console.log(res)
 		}).catch(res => {
 			console.log(res)
@@ -25,6 +35,12 @@ const actions = {
 }
 
 const mutations = {
+	SET_PROVIDERS (state, params) {
+		state.providers = params.providers
+	},
+	SET_PROVIDER (state, params) {
+		state.provider = params.provider
+	}
 }
 
 export default {
