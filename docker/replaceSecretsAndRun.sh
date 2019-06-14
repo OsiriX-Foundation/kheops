4d2ec6130b59cceeb2e6b18418bbc124d5ce0ef4
gerome12 <gerome.pasquier@gmail.com>
2019-06-14 15:14:06 +0200

    update filebeat

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -133,36 +133,20 @@ if ! [ -z "$KHEOPS_AUTHORIZATION_ENABLE_ELASTIC" ]; then
         echo "Start init filebeat"
         missing_env_var_secret=false
 
-        if [[ -z $KHEOPS_AUTHORIZATION_ELASTIC_NAME ]]; then
-          echo "Missing KHEOPS_AUTHORIZATION_ELASTIC_NAME environment variable"
+        if [[ -z $KHEOPS_AUTHORIZATION_ELASTIC_INSTANCE ]]; then
+          echo "Missing $KHEOPS_AUTHORIZATION_ELASTIC_INSTANCE environment variable"
           missing_env_var_secret=true
         else
-           echo -e "environment variable KHEOPS_AUTHORIZATION_ELASTIC_NAME \e[92mOK\e[0m"
-           sed -i "s|\${elastic_name}|$KHEOPS_AUTHORIZATION_ELASTIC_NAME|" /etc/filebeat/filebeat.yml
+           echo -e "environment variable $KHEOPS_AUTHORIZATION_ELASTIC_INSTANCE \e[92mOK\e[0m"
+           sed -i "s|\${instance}|$KHEOPS_AUTHORIZATION_ELASTIC_INSTANCE|" /etc/filebeat/filebeat.yml
         fi
 
-        if [[ -z $KHEOPS_AUTHORIZATION_ELASTIC_TAGS ]]; then
-          echo "Missing KHEOPS_AUTHORIZATION_ELASTIC_TAGS environment variable"
+        if [[ -z $KHEOPS_AUTHORIZATION_LOGSTASH_URL ]]; then
+          echo "Missing $KHEOPS_AUTHORIZATION_LOGSTASH_URL environment variable"
           missing_env_var_secret=true
         else
-           echo -e "environment variable KHEOPS_AUTHORIZATION_ELASTIC_TAGS \e[92mOK\e[0m"
-           sed -i "s|\${elastic_tags}|$KHEOPS_AUTHORIZATION_ELASTIC_TAGS|" /etc/filebeat/filebeat.yml
-        fi
-
-        if [[ -z $KHEOPS_AUTHORIZATION_ELASTIC_URL ]]; then
-          echo "Missing KHEOPS_AUTHORIZATION_ELASTIC_URL environment variable"
-          missing_env_var_secret=true
-        else
-           echo -e "environment variable KHEOPS_AUTHORIZATION_ELASTIC_URL \e[92mOK\e[0m"
-           sed -i "s|\${elastic_url}|$KHEOPS_AUTHORIZATION_ELASTIC_URL|" /etc/filebeat/filebeat.yml
-        fi
-
-        if [[ -z $KHEOPS_AUTHORIZATION_KIBANA_URL ]]; then
-          echo "Missing KHEOPS_AUTHORIZATION_KIBANA_URL environment variable"
-          missing_env_var_secret=true
-        else
-           echo -e "environment variable KHEOPS_AUTHORIZATION_KIBANA_URL \e[92mOK\e[0m"
-           sed -i "s|\${kibana_url}|$KHEOPS_AUTHORIZATION_KIBANA_URL|" /etc/filebeat/filebeat.yml
+           echo -e "environment variable $KHEOPS_AUTHORIZATION_LOGSTASH_URL \e[92mOK\e[0m"
+           sed -i "s|\${logstash_url}|$KHEOPS_AUTHORIZATION_LOGSTASH_URL|" /etc/filebeat/filebeat.yml
         fi
 
         #if missing env var or secret => exit
