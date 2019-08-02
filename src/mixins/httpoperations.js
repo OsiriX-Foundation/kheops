d0c9a40aa485345529a7adb34cf64582a96afa9e
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-08-02 14:15:52 +0200

    Fix bug

--- src/mixins/httpoperations.js
+++ src/mixins/httpoperations.js
@@ -3,7 +3,7 @@ export default {
 		let tabQueries = []
 		for (var key in queries) {
 			if (typeof queries[key] !== 'function' && typeof queries[key] !== 'object') {
-				tabQueries.push(`${key}=${queries[key]}`)
+				tabQueries.push(`${encodeURIComponent(key)}=${encodeURIComponent(queries[key])}`)
 			} else if (typeof queries[key] !== 'function' && queries[key].constructor === Array) {
 				queries[key].forEach(value => {
 					tabQueries.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
