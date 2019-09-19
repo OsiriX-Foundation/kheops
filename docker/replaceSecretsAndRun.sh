10c3b08a41873a123d89b742fd6722c17b03611f
Joël Spaltenstein <spalte@naturalimage.ch>
2019-09-19 16:42:59 +0200

    ignore kubernetes secrets

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -41,10 +41,14 @@ fi
 #get secrets and verify content
 for f in ${SECRET_FILE_PATH}/*
 do
+  filename=$(basename "$f")
+  
+  if [ "$filename" = "kubernetes.io" ]; then
+    continue
+  fi
+  
   word_count=$(wc -w $f | cut -f1 -d" ")
   line_count=$(wc -l $f | cut -f1 -d" ")
-
-  filename=$(basename "$f")
 
   if [ ${word_count} != 1 ] || [ ${line_count} != 1 ]; then
     echo Error with secret $filename. He contains $word_count word and $line_count line
