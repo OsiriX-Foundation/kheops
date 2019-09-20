382fc266d8645a93502260333cbaa4dd634b46c3
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-09-20 15:38:24 +0200

    Eslint ok

--- src/mixins/customdicom.js
+++ src/mixins/customdicom.js
@@ -1,10 +1,10 @@
 export default {
-	'name2customdicom': {
-		'SumFavorites': '00012345',
-		'SumComments': '00012346'
-	},
-	'customdicom2name': {
-		'00012345': 'SumFavorites',
-		'00012346': 'SumComments'
-	}
-}
+  name2customdicom: {
+    SumFavorites: '00012345',
+    SumComments: '00012346',
+  },
+  customdicom2name: {
+    '00012345': 'SumFavorites',
+    '00012346': 'SumComments',
+  },
+};
