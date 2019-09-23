950c8e0ad4505814701515a2cdbe1aec495e5ca2
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-09-23 15:32:44 +0200

    eslint

--- src/router/http.js
+++ src/router/http.js
@@ -1,18 +1,13 @@
-import axios from 'axios'
-import { serverURL } from '@/app_config'
-import Vue from 'vue'
+import axios from 'axios';
+import Vue from 'vue';
+import { serverURL } from '@/app_config';
 
-export var HTTP = axios.create({ baseURL: serverURL })
+export const HTTP = axios.create({ baseURL: serverURL });
 
-HTTP.interceptors.request.use(function (config) {
-	config.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`
-	return config
-}, function (error) {
-	return Promise.reject(error)
-})
+HTTP.interceptors.request.use((config) => {
+  const tmpconfig = config;
+  tmpconfig.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`;
+  return tmpconfig;
+}, (error) => Promise.reject(error));
 
-HTTP.interceptors.response.use(function (response) {
-	return response
-}, function (error) {
-	return Promise.reject(error)
-})
+HTTP.interceptors.response.use((response) => response, (error) => Promise.reject(error));
