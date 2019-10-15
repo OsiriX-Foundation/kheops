3e861adf90bba68d66e2e862bc87b8ffbceec3c6
gerome12 <gerome.pasquier@gmail.com>
2019-10-15 09:33:36 +0200

    rollback

--- src/main/java/online/kheops/proxy/wadors/WadoRsResource.java
+++ src/main/java/online/kheops/proxy/wadors/WadoRsResource.java
@@ -89,14 +89,6 @@ public final class WadoRsResource {
         return webAccess(studyInstanceUID, seriesInstanceUID, AuthorizationToken.fromAuthorizationHeader(authorizationHeader));
     }
     
-    @GET
-    @Path("/password/dicomweb/studies/{studyInstanceUID:([0-9]+[.])*[0-9]+}/series/{seriesInstanceUID:([0-9]+[.])*[0-9]+}/thumbnail")
-    public Response wadoInstanceFrame(@HeaderParam(AUTHORIZATION) String authorizationHeader,
-                                 @PathParam("studyInstanceUID") String studyInstanceUID,
-                                 @PathParam("seriesInstanceUID") String seriesInstanceUID) {
-        return webAccess(studyInstanceUID, seriesInstanceUID, AuthorizationToken.fromAuthorizationHeader(authorizationHeader));
-    }
-    
     //@GET
     //@Path("/password/dicomweb/studies/{studyInstanceUID:([0-9]+[.])*[0-9]+}/series/{seriesInstanceUID:([0-9]+[.])*[0-9]+}/instances")
     //public Response wadoInstances(@HeaderParam(AUTHORIZATION) String authorizationHeader,
