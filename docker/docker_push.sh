7bc615a92672ac652d8e16d23d7f7720db0589ee
gerome12 <gerome.pasquier@gmail.com>
2019-12-19 11:01:32 +0100

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -7,11 +7,8 @@ mv metricbeat docker
 mv liquibase docker
 
 
-#docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheops-authorization:$TRAVIS_BRANCH
-
-#docker push osirixfoundation/kheops-authorization:$TRAVIS_BRANCH
-
-echo $TRAVIS_TAG
+docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheops-authorization:$TRAVIS_BRANCH
+docker push osirixfoundation/kheops-authorization:$TRAVIS_BRANCH
 
 if [ "$TRAVIS_TAG" != "" ]; then
     docker build ./docker/ --build-arg VCS_REF=`git rev-parse --short HEAD` -t osirixfoundation/kheops-authorization:$TRAVIS_TAG
