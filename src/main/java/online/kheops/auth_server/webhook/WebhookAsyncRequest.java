b82bac28b8405d02ffd5ea0f97ffb0fbf222532a
gerome12 <gerome.pasquier@gmail.com>
2020-01-22 14:32:51 +0100

    Update WebhookAsyncRequest.java

--- src/main/java/online/kheops/auth_server/webhook/WebhookAsyncRequest.java
+++ src/main/java/online/kheops/auth_server/webhook/WebhookAsyncRequest.java
@@ -33,8 +33,9 @@ public class WebhookAsyncRequest<T> {
     }
 
     private void request(int cnt) {
-        Entity s = Entity.json(data);
+        Entity<T> s = Entity.json(data);
         String a = s.getEntity().toString();
+
         Future<Response> f = CLIENT.target(webhook.getUrl()).request().async()
                 .post(Entity.json(data),
                         new WebhooksCallbacks<T>(webhook, isManualTrigger, cnt, this));
