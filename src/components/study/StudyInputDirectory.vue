ff84c4b0c7730a2459eec8cea684e94addb4bf1b
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-03-24 10:51:25 +0100

    Component input to import files or directory

---
+++ src/components/study/StudyInputDirectory.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    id="directory"
+    ref="inputdir"
+    type="file"
+    name="file"
+    class="inputfile"
+    webkitdirectory
+    :disabled="sendingFiles"
+    @change="inputLoadDirectories"
+  >
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'StudyInputDirectory',
+  computed: {
+    ...mapGetters({
+      sendingFiles: 'sending',
+    }),
+  },
+  methods: {
+    inputLoadDirectories() {
+      if (this.$refs.inputdir.files.length > 0) {
+        const filesFromInput = this.$refs.inputdir.files;
+        this.$emit('loaddirectories', filesFromInput);
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/study/StudyInputFile.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    id="file"
+    ref="inputfiles"
+    type="file"
+    name="file"
+    class="inputfile"
+    multiple
+    :disabled="sendingFiles"
+    @change="inputLoadFiles"
+  >
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'StudyInputFile',
+  computed: {
+    ...mapGetters({
+      sendingFiles: 'sending',
+    }),
+  },
+  methods: {
+    inputLoadFiles() {
+      if (this.$refs.inputfiles.files.length > 0) {
+        const filesFromInput = this.$refs.inputfiles.files;
+        this.$emit('loadfiles', filesFromInput);
+      }
+    },
+  },
+};
+</script>
