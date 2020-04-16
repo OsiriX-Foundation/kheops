da7c1c463e786d890ae27687515591f2d0f32df8
Joël Spaltenstein <spalte@naturalimage.ch>
2020-04-16 23:27:55 +0200

    use KHEOPS_ROOT_URL

--- docker/replaceSecretsAndRun.sh
+++ docker/replaceSecretsAndRun.sh
@@ -33,12 +33,8 @@ if [ -z "$KHEOPS_AUTHORIZATION_PATH" ]; then
     echo "Missing KHEOPS_AUTHORIZATION_PATH environment variable"
     missing_env_var_secret=true
 fi
-if [ -z "$KHEOPS_ROOT_SCHEME" ]; then
-    echo "Missing KHEOPS_ROOT_SCHEME environment variable"
-    missing_env_var_secret=true
-fi
-if [ -z "$KHEOPS_ROOT_HOST" ]; then
-    echo "Missing KHEOPS_ROOT_HOST environment variable"
+if [ -z "$KHEOPS_ROOT_URL" ]; then
+    echo "Missing $KHEOPS_ROOT_URL environment variable"
     missing_env_var_secret=true
 fi
 if [ -z "$KHEOPS_API_PATH" ]; then
@@ -82,12 +78,7 @@ done
 sed -i "s|\${kheops_pacs_url}|http://$KHEOPS_PACS_PEP_HOST:$KHEOPS_PACS_PEP_PORT|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_authorization_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT$KHEOPS_AUTHORIZATION_PATH|" ${REPLACE_FILE_PATH}
 sed -i "s|\${kheops_client_dicomwebproxyclientid}|$KHEOPS_CLIENT_DICOMWEBPROXYCLIENTID|" ${REPLACE_FILE_PATH}
-
-if { [ "$KHEOPS_ROOT_SCHEME" = "http" ] && [ "$KHEOPS_ROOT_PORT" = "80" ]; } || { [ "$KHEOPS_ROOT_SCHEME" = "https" ] && [ "$KHEOPS_ROOT_PORT" = "443" ]; }; then
-    sed -i "s|\${kheops_root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" ${REPLACE_FILE_PATH}
-else
-    sed -i "s|\${kheops_root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST:$KHEOPS_ROOT_PORT|" ${REPLACE_FILE_PATH}
-fi
+sed -i "s|\${kheops_root_url}|$KHEOPS_ROOT_URL|" ${REPLACE_FILE_PATH}
 echo "Ending setup NGINX secrets and env var"
 
 #######################################################################################
