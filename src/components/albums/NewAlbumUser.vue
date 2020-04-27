b65e40a2bf6a0a87ab319198dae4b0c83d743cea
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-04-27 09:54:04 +0200

    Show name

--- src/components/albums/NewAlbumUser.vue
+++ src/components/albums/NewAlbumUser.vue
@@ -11,7 +11,7 @@
         :key="user.email"
       >
         <td>
-          {{ user|getUsername }}
+          {{ user|getUsername }} {{ user.name !== undefined ? `- ( ${user.name} )` : '' }}
           <span
             v-if="user.is_admin"
           >
