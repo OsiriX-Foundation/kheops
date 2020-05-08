b2d53d22479cbee821709af9ad894517a78c988c
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-05-08 11:09:26 +0200

    Accept service name in Regex #69

--- src/mixins/validator.js
+++ src/mixins/validator.js
@@ -5,7 +5,8 @@ export const validator = {
     checkUrl(url) {
       const pattern = new RegExp('^https?:\\/\\/' // protocol
         + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
-        + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+        + '((\\d{1,3}\\.){3}\\d{1,3}) |' // OR ip (v4) address
+        + '([a-z\\d]([a-z\\d-]*[a-z\\d])*))' // OR service name
         + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port and path
         + '(\\?[;&a-z\\d%_.~+=-]*)?' // query string
         + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
