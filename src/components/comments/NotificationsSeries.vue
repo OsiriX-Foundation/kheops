a92c62fcec9b971ae3a860a7cd33108473878f29
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-05-15 14:47:40 +0200

    comment.origin to comment.source and remove NEW_REPORT

--- src/components/comments/NotificationsSeries.vue
+++ src/components/comments/NotificationsSeries.vue
@@ -4,13 +4,13 @@
       v-if="comment.mutation_type === 'IMPORT_SERIES'"
       class=" flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.theseries') }} <b>{{ comment.series.description ? comment.series.description : comment.series.UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.theseries') }} <b>{{ comment.series.description ? comment.series.description : comment.series.UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
     <div
       v-if="comment.mutation_type === 'REMOVE_SERIES'"
       class=" flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.theseries') }} <b>{{ comment.series.description ? comment.series.description : comment.series.UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.theseries') }} <b>{{ comment.series.description ? comment.series.description : comment.series.UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
   </span>
 </template>
--- src/components/comments/NotificationsStudy.vue
+++ src/components/comments/NotificationsStudy.vue
@@ -4,25 +4,25 @@
       v-if="comment.mutation_type === 'IMPORT_STUDY'"
       class="flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
     <div
       v-if="comment.mutation_type === 'REMOVE_STUDY'"
       class="flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
     <div
       v-if="comment.mutation_type === 'ADD_FAV' && comment.study"
       class=" flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.addfavorite') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.addfavorite') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
     <div
       v-if="comment.mutation_type === 'REMOVE_FAV' && comment.study"
       class=" flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.origin|getUsername }}</i> {{ $t('comment.removefavorite') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removefavorite') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
     </div>
   </span>
 </template>
