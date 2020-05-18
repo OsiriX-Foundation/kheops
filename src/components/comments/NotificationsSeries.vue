8647b33cf0bb3c8e14cdb0672b3c62fba4a08af7
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-05-18 16:56:38 +0200

    Show present or not present serie, makes the logs unreadable if too many series

--- src/components/comments/NotificationsSeries.vue
+++ src/components/comments/NotificationsSeries.vue
@@ -10,6 +10,11 @@
         {{ $t('comment.newreport', {user: getName(comment.source), reportname: comment.source.report_provider.name, serie: comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID, study: comment.study.description ? comment.study.description : comment.study.UID}) }}
       </span>
       <span
+        v-else-if="comment.source.capability_token !== undefined"
+      >
+        {{ $t('comment.importserietoken', {user: getName(comment.source), serie: comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID, study: comment.study.description ? comment.study.description : comment.study.UID}) }}
+      </span>
+      <span
         v-else
       >
         <i>{{ comment.source|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.theseries') }} <b>{{ comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
@@ -18,19 +23,28 @@
         v-if="comment.study.series[0].is_present_in_album === false"
         class="text-warning"
       >
-        {{ $t('comment.serienotpresentinalbum') }}
+        <br>{{ $t('comment.serienotpresentinalbum') }}
       </span>
     </div>
     <div
       v-if="comment.mutation_type === 'REMOVE_SERIES'"
       class=" flex-grow-1 bd-highlight"
     >
-      <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.theseries') }} <b>{{ comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <span
+        v-if="comment.source.capability_token !== undefined"
+      >
+        {{ $t('comment.removeserietoken', {user: getName(comment.source), serie: comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID, study: comment.study.description ? comment.study.description : comment.study.UID}) }}
+      </span>
+      <span
+        v-else
+      >
+        <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.theseries') }} <b>{{ comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID }}</b> {{ $t('comment.in') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      </span>
       <span
         v-if="comment.study.series[0].is_present_in_album === true"
         class="text-warning"
       >
-        {{ $t('comment.seriepresentinalbum') }}
+        <br>{{ $t('comment.seriepresentinalbum') }}
       </span>
     </div>
   </span>
--- src/components/comments/NotificationsStudy.vue
+++ src/components/comments/NotificationsStudy.vue
@@ -5,12 +5,46 @@
       class="flex-grow-1 bd-highlight"
     >
       <i>{{ comment.source|getUsername }}</i> {{ $t('comment.imported') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <span
+        v-if="lengthNotPresent(comment.study.series) === comment.study.series.length"
+        class="text-warning"
+      >
+        <br>{{ $t('comment.nomoreseriesinstudy', {study: comment.study.description ? comment.study.description : comment.study.UID}) }}
+      </span>
+      <span
+        v-else
+      >
+        <span
+          v-for="serie in isNotPresent(comment.study.series)"
+          :key="serie.id"
+          class="text-warning"
+        >
+          <br>{{ $t('comment.theserienotpresentinalbum', {serie: comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID}) }}
+        </span>
+      </span>
     </div>
     <div
       v-if="comment.mutation_type === 'REMOVE_STUDY'"
       class="flex-grow-1 bd-highlight"
     >
       <i>{{ comment.source|getUsername }}</i> {{ $t('comment.removed') }} {{ $t('comment.thestudy') }} <b>{{ comment.study.description ? comment.study.description : comment.study.UID }}</b>
+      <span
+        v-if="lengthPresent(comment.study.series) === comment.study.series.length"
+        class="text-warning"
+      >
+        <br>{{ $t('comment.allseriesinstudy', {study: comment.study.description ? comment.study.description : comment.study.UID}) }}
+      </span>
+      <span
+        v-else
+      >
+        <span
+          v-for="serie in isPresent(comment.study.series)"
+          :key="serie.id"
+          class="text-warning"
+        >
+          <br>{{ $t('comment.theseriepresentinalbum', {serie: comment.study.series[0].description ? comment.study.series[0].description : comment.study.series[0].UID}) }}
+        </span>
+      </span>
     </div>
     <div
       v-if="comment.mutation_type === 'ADD_FAV' && comment.study"
@@ -34,6 +68,22 @@ export default {
       type: Object,
       required: true,
       default: () => {},
+    },
+  },
+  methods: {
+    lengthNotPresent(series) {
+      const seriesNotPresent = this.isNotPresent(series);
+      return seriesNotPresent.length;
+    },
+    isNotPresent(series) {
+      return series.filter((serie) => serie.is_present_in_album === false);
+    },
+    lengthPresent(series) {
+      const seriesPresent = this.isPresent(series);
+      return seriesPresent.length;
+    },
+    isPresent(series) {
+      return series.filter((serie) => serie.is_present_in_album === true);
     },
   },
 };
