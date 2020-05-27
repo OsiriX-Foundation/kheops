d70733cb0da5c872aa7584d74be9263c2a8c3a30
gerome12 <gerome.pasquier@gmail.com>
2020-05-27 14:10:19 +0200

    Update WebhookAttemptResponse.java

--- src/main/java/online/kheops/auth_server/webhook/WebhookAttemptResponse.java
+++ src/main/java/online/kheops/auth_server/webhook/WebhookAttemptResponse.java
@@ -4,20 +4,22 @@ import online.kheops.auth_server.entity.WebhookAttempt;
 
 import javax.xml.bind.annotation.XmlElement;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class WebhookAttemptResponse {
 
     @XmlElement(name = "status")
     private Long status;
     @XmlElement(name = "time")
-    private LocalDateTime time;
+    private String time;
     @XmlElement(name = "attempt")
     private Long attempt;
 
     private WebhookAttemptResponse() { /*Empty*/ }
 
     public WebhookAttemptResponse(WebhookAttempt webhookAttempt) {
-        time = webhookAttempt.getTime();
+        time = ZonedDateTime.of(webhookAttempt.getTime(), ZoneOffset.UTC).toString();
         status = webhookAttempt.getStatus();
         attempt = webhookAttempt.getAttempt();
     }
