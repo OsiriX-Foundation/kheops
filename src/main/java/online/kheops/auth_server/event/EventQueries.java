e35b40d7999eb87307e748e6e757e2c9411c218c
gerome12 <gerome.pasquier@gmail.com>
2020-06-05 13:36:54 +0200

    Update EventQueries.java

--- src/main/java/online/kheops/auth_server/event/EventQueries.java
+++ src/main/java/online/kheops/auth_server/event/EventQueries.java
@@ -65,6 +65,8 @@ public class EventQueries {
         final Root<Mutation> mutation = c.from(Mutation.class);
         c.select(cb.countDistinct(mutation));
         Join<Mutation, Series> seriesJoin = mutation.join("series", JoinType.LEFT);
+        Join<Mutation, User> userJoin = mutation.join("user", JoinType.LEFT);
+        Join<Mutation, User> toUserJoin = mutation.join("toUser", JoinType.LEFT);
 
         c.where(cb.and(filters.toArray(new Predicate[0])));
         TypedQuery<Long> q = em.createQuery(c);
