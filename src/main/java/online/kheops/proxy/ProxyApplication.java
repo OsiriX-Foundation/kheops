42f92275cd9c9315d2587a541a412fdb72d57b5f
Joël Spaltenstein <spalte@naturalimage.ch>
2020-06-10 00:37:12 +0200

    clean up provider registration

--- src/main/java/online/kheops/proxy/ProxyApplication.java
+++ src/main/java/online/kheops/proxy/ProxyApplication.java
@@ -1,28 +1,23 @@
 package online.kheops.proxy;
 
 import online.kheops.proxy.filter.CORSFilter;
+import online.kheops.proxy.filter.FixTypeQuoteFilter;
+import online.kheops.proxy.filter.IncludefieldFilter;
 import online.kheops.proxy.marshaller.*;
 import online.kheops.proxy.multipart.MultipartStreamingProvider;
 import online.kheops.proxy.ohif.OHIFMetadataResource;
 import online.kheops.proxy.stow.resource.Resource;
 import online.kheops.proxy.wadors.*;
 import online.kheops.proxy.wadouri.WadoUriResource;
-import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.Providers;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 @ApplicationPath("/*")
 public class ProxyApplication extends Application {
-
-    @Context
-    Providers providers;
 
     @Override
     public Set<Class<?>> getClasses() {
@@ -35,27 +30,17 @@ public class ProxyApplication extends Application {
         set.add(WadoRSStudyInstance.class);
         set.add(WadoRSSeriesInstances.class);
         set.add(OHIFMetadataResource.class);
+        set.add(MultiPartFeature.class);
+        set.add(JSONAttributesProvider.class);
+        set.add(JSONAttributesListProvider.class);
+        set.add(XMLAttributesProvider.class);
+        set.add(XMLAttributesListProvider.class);
+        set.add(MultipartStreamingProvider.class);
+        set.add(StreamingAttributesProvider.class);
+        set.add(CORSFilter.class);
+        set.add(FixTypeQuoteFilter.class);
+        set.add(IncludefieldFilter.class);
+
         return set;
-    }
-
-    @Override
-    public Set<Object> getSingletons() {
-        Set<Object> set = new HashSet<>();
-        set.add(new JSONAttributesProvider());
-        set.add(new JSONAttributesListProvider());
-        set.add(new XMLAttributesProvider());
-        set.add(new XMLAttributesListProvider(providers));
-        set.add(new MultipartStreamingProvider(providers));
-        set.add(new StreamingAttributesProvider(providers));
-        set.add(new CORSFilter());
-        return set;
-    }
-
-    @Override
-    public Map<String, Object> getProperties() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ServerProperties.PROVIDER_CLASSNAMES, "org.glassfish.jersey.media.multipart.MultiPartFeature");
-
-        return props;
     }
 }
