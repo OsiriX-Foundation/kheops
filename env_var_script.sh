1930f08587c8f5588fac412a9a65d40007787650
gerome12 <gerome.pasquier@gmail.com>
2020-10-19 16:28:51 +0200

    Update env_var_script.sh

--- env_var_script.sh
+++ env_var_script.sh
@@ -133,7 +133,7 @@ if /usr/bin/find "/etc/nginx/kheops/" -mindepth 1 -maxdepth 1 -type f -print -qu
     find "/etc/nginx/kheops/" -follow -type f -print | sort -n | while read -r f; do
         case "$f" in
             *.conf)
-                remplace_in_file($f)
+                remplace_in_file ${f}
                 ;;
             *) echo >&3 "$0: Ignoring $f";;
         esac
@@ -144,6 +144,6 @@ else
     echo >&3 "$0: No files found in /etc/nginx/kheops/, skipping configuration"
 fi
 
-remplace_in_file(/etc/nginx/conf.d/kheops.conf)
+remplace_in_file /etc/nginx/conf.d/kheops.conf
 
 echo "Ending setup NGINX secrets and env var"
