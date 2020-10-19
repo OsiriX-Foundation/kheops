2465ded7485fa150d2817c2766548f30b53b698c
gerome12 <gerome.pasquier@gmail.com>
2020-10-19 16:15:54 +0200

    dev

--- env_var_script.sh
+++ env_var_script.sh
@@ -142,7 +142,6 @@ else
 fi
 
 
-
 #get env var
 chmod a+w /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${root_url}|$proto$host|" /etc/nginx/conf.d/kheops.conf
@@ -155,7 +154,6 @@ sed -i "s|\${kheopsWebUI_url}|http://$KHEOPS_UI_HOST:$KHEOPS_UI_PORT|" /etc/ngin
 
 sed -i "s|\${server_name}|$roothost|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${keycloak_url}|$proto$hostport|g" /etc/nginx/conf.d/kheops.conf
-
 
 
 echo "Ending setup NGINX secrets and env var"
