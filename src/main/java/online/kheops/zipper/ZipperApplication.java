b40d2be6d72729b16b1e2c304b83a44e342645b9
gerome12 <gerome.pasquier@gmail.com>
2020-12-18 15:06:53 +0100

    Update ZipperApplication.java

--- src/main/java/online/kheops/zipper/ZipperApplication.java
+++ src/main/java/online/kheops/zipper/ZipperApplication.java
@@ -1,9 +1,15 @@
 package online.kheops.zipper;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+
 
 @ApplicationPath("/*")
-public final class ZipperApplication extends Application {
+public final class ZipperApplication extends ResourceConfig {
+
+    public ZipperApplication() {
+        property("jersey.config.server.provider.classnames", "org.glassfish.jersey.media.multipart.MultiPartFeature,online.kheops.zipper.filter.CacheFilterFactory");
+        packages(true, "online.kheops.zipper");
+    }
 
 }
