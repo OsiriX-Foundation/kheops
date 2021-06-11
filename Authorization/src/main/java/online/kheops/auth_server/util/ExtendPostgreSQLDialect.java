f58c70a5b2d9c73f6ea47a257e29c1b581f9e4de
gerome12 <gerome.pasquier@gmail.com>
2021-06-11 10:47:14 +0200

    rename class ExtendPostgreSQLDialect

--- Authorization/src/main/java/online/kheops/auth_server/util/ExtendPostgreSQLDialect.java
+++ Authorization/src/main/java/online/kheops/auth_server/util/ExtendPostgreSQLDialect.java
@@ -4,9 +4,9 @@ import org.hibernate.dialect.PostgreSQLDialect;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
 
-public class extendPostgreSQLDialect extends PostgreSQLDialect {
+public class ExtendPostgreSQLDialect extends PostgreSQLDialect {
 
-    public extendPostgreSQLDialect() {
+    public ExtendPostgreSQLDialect() {
         super();
         registerFunction("array_agg", new StandardSQLFunction("array_agg", StandardBasicTypes.STRING));
         registerFunction("SOUNDEX", new StandardSQLFunction("SOUNDEX", StandardBasicTypes.LONG));
